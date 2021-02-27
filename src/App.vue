3ca7681aedbdfaeb6bfb4171d3f2f9086e40f256
Vladdizlaw <vladdizlaw@gmail.com>
2021-02-28 00:26:22 +0300

    add localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,192 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="wraper">
+    <div class="forinput">
+      <p>ticker</p>
+      <input
+        type="text"
+        v-model="ticker"
+        @keydown.enter="validation() ? btnAdd() : pass"
+        class="generalinput"
+      />
+      <div v-if="messages.mes" class="messagesfield">
+        <p
+          v-for="(mes, ind) in messages.mes"
+          :key="ind"
+          @click="
+            ticker = mes;
+            validation() ? btnAdd() : pass;
+          "
+        >
+          {{ mes }}
+        </p>
+      </div>
+      <div v-if="messages.errors" class="errorsfield">
+        <p>{{ messages.errors }}</p>
+      </div>
+      <button class="btn-add" v-on:click="validation() ? btnAdd() : pass">
+        add
+      </button>
+    </div>
+    <hr />
+    <div class="filter">
+      <input type="text" class="filterinput">
+      <button class="prevpage">prev </button>
+      <button class="nextpage"> next </button>
+    </div>
+    <div class="forwallets">
+      <div
+        @click="selected === t ? (selected = null) : (selected = t)"
+        v-for="(t, i) in tickers"
+        :key="i"
+        class="walletblock"
+        :class="{ active: t === selected }"
+      >
+        <p>{{ t.name }}/USD</p>
+        <p>{{ t.fullName }}</p>
+        <h1>{{ t.price }}</h1>
+        <h4 @click.stop="btnDelete(i)">Delete</h4>
+      </div>
+    </div>
+    <hr />
+    <div v-if="selected" class="outputdisplay">
+      <button @click="selected = null" class="closeoutput">X</button>
+
+      <div class="graph">
+        <div
+          v-for="key in Object.keys(maxHeightBar)"
+          :key="key"
+          :title="key"
+          :style="{ height: maxHeightBar[key] + '%' }"
+          class="bar"
+        ></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      ticker: "",
+      tickers: [],
+      coinsList: null,
+      selected: null,
+      graph: {},
+      messages: { mess: null, errors: null }
+    };
+  },
+  watch: {
+    ticker: function() {
+      if (this.tickersName.includes(this.ticker?.toUpperCase())) {
+        this.messages.errors = "This crypto is selected alredy";
+      } else {
+        this.messages.errors = null;
+      }
+
+      this.messages.mes = this.tickersMessages;
+    },
+    
+  },
+  computed: {
+    tickersMessages() {
+      if (this.ticker != "") {
+        return Object.keys(this.coinsList)
+          .filter(el => el.includes(this.ticker?.toUpperCase()))
+          .sort((a, b) => {
+            return a.length - b.length;
+          })
+          .slice(0, 5);
+      }
+      return null;
+    },
+
+    tickersName() {
+      return this.tickers.map(el => el.name);
+    },
+
+    maxHeightBar() {
+      let result = {};
+
+      if (this.graph[this.selected.name]) {
+        let max = Math.max(...this.graph[this.selected.name]);
+        let min = Math.min(...this.graph[this.selected.name]);
+        this.graph[this.selected.name].forEach(el => {
+          let procent = 5 + ((el - min) * 95) / (max - min);
+          result[el] = procent;
+        });
+      }
+      
+      return result;
+    }
+  },
+  async created() {
+    const resp = await fetch(
+      "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
+    );
+    const data = await resp.json();
+    this.coinsList = data.Data;
+    localStorage.tickersList?this.tickers=JSON.parse(localStorage.getItem('tickersList')):null
+    this.tickers.forEach(el=>{
+      this.updatePrice(el.name)
+    })
+    
+    
+
+   
+  },
+  updated(){
+    localStorage.setItem('tickersList',JSON.stringify(this.tickers))
+   
+  },
+  methods: {
+    updatePrice(tickerName){
+      setInterval(async () => {
+        let f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=e471e53bb40142e09d4decbeb1e49a9943383e0b588a08f7c6f60d118fbe82ab`
+        );
+
+        const data = await f.json();
+
+        this.tickers.find(el => el.name === tickerName)?this.tickers.find(el => el.name === tickerName).price= data.USD:null;
+
+        this.graph[tickerName]
+          ? this.graph[tickerName].push(data.USD)
+          : (this.graph[tickerName] = []);
+      }, 5000);
+    },
+    chooseMes(mes) {
+      this.ticker = mes;
+    },
+    validation() {
+      if (
+        this.coinsList[this.ticker?.toUpperCase()] &&
+        !this.tickersName.includes(this.ticker.toUpperCase())
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    btnAdd() {
+      const added = {
+        name: this.ticker.toUpperCase(),
+        price: "--------",
+        fullName: this.coinsList[this.ticker?.toUpperCase()]["FullName"]
+      };
+      this.tickers.push(added);
+      this.updatePrice(this.ticker)
+      this.ticker = "";
+      
+      
+    },
+    btnDelete(i) {
+      this.tickers.splice(i, 1);
+    }
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./style.css"></style>
