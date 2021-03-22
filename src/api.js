bcf15e3f9353857d6421c3f9d1c780d9c34d844e
Vladdizlaw <vladdizlaw@gmail.com>
2021-03-23 00:31:52 +0300

    Badly working switch to BTC/USD

--- src/api.js
+++ src/api.js
@@ -3,6 +3,30 @@ const tickersHandlers = new Map();
 const socket = new WebSocket(
   `wss://streamer.cryptocompare.com/v2?api_key=${API}`
 );
+let BTCPrice=undefined
+let flag=false
+function setBtcPrice(){
+  if (tickersHandlers.has('BTC')){
+    console.log('ret')
+    return
+  }
+  const message = JSON.stringify({
+    action: "SubAdd",
+    subs: [`5~CCCAGG~BTC~USD`]
+  });
+  if (socket.readyState == WebSocket.OPEN) {
+    socket.send(message);
+  }
+  socket.addEventListener('open',()=>{
+    socket.send(message)
+  },{once:true})
+  socket.addEventListener('message',e=>{
+   const  {PRICE:newPrice} = JSON.parse(e.data)
+  newPrice?BTCPrice=newPrice:null
+  console.log(newPrice)
+  })
+}
+
 function subscribeToTickerOnWebsocket(tickerName) {
   const message = JSON.stringify({
     action: "SubAdd",
@@ -19,22 +43,54 @@ function subscribeToTickerOnWebsocket(tickerName) {
     { once: true }
   );
   socket.addEventListener("message", e => {
-    const { TYPE: type, FROMSYMBOL: currency, PRICE: price } = JSON.parse(
+    let {INFO:info, TYPE: type, FROMSYMBOL: currency, PRICE: price } = JSON.parse(
       e.data
     );
 
-    if (type != "5") {
+    if (type != "5" && type != '500') {
       return;
     }
-    const handlers = tickersHandlers.get(currency) ?? [];
+    if(tickerName=='BTC'){
 
+      BTCPrice=price
+    }else { setBtcPrice() }
+    if (tickerName!='BTC'&& type=='500' && info&&info.includes('We have not integrated ')){
+
+        let message = JSON.stringify({
+        action: "SubAdd",
+        subs: [`5~CCCAGG~${tickerName}~BTC`]
+      });
+      if (socket.readyState == WebSocket.OPEN) {
+        socket.send(message);
+      }
+      socket.addEventListener('open',()=>{
+        socket.send(message)
+      },{once:true})
+      socket.addEventListener ('message', e=>{
+       const  {PRICE:newPrice} = JSON.parse(e.data)
+      
+        price=newPrice*BTCPrice
+        flag=true;
+       console.log('BTC:',BTCPrice,price)
+      
+      })
+      
+    } 
+    const handlers = tickersHandlers.get(currency) ?? [];
+  
     handlers.forEach(handler => handler(price));
   });
 }
+
 function unsubscribeFromTickerOnWebsocket(tickerName) {
+  let curr='USD'
+  if (flag){
+    curr='BTC'
+    flag=false
+  }
   const message = JSON.stringify({
     action: "SubRemove",
-    subs: [`5~CCCAGG~${tickerName}~USD`]
+    subs: [`5~CCCAGG~${tickerName}~${curr}`]
   });
   if (socket.readyState == WebSocket.OPEN) {
     socket.send(message);
@@ -48,27 +104,6 @@ function unsubscribeFromTickerOnWebsocket(tickerName) {
   );
 }
 
-/*export async function getCurrencyData() {
-  if (tickersHandlers.size === 0) {
-    return;
-  }
-  const tickerListNames = [...tickersHandlers.keys()].join(",");
-
-  const res = await fetch(
-    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tickerListNames}&tsyms=USD&api_key=${API}`
-  );
-  const js = await res.json();
-
-  const updatedPrices = Object.fromEntries(
-    Object.entries(js).map(([name, price]) => [name, price["USD"]])
-  );
-  Object.entries(updatedPrices).forEach(([currency, price]) => {
-    //Запускаем каждую функцию в tickerHandlerе
-    const handlers = tickersHandlers.get(currency) ?? [];
-    handlers.forEach(handler => handler(price));
-  });
-  return updatedPrices;
-}*/
 export const subscribeToTicker = (ticker, cb) => {
   //подписка на тикер, дописывает в tickerHandler, ticker и массив функций для вызова
   //если тикера нет ,заводит в tickerHandler пустой массив функций
@@ -76,6 +111,7 @@ export const subscribeToTicker = (ticker, cb) => {
   tickersHandlers.set(ticker, [...subscriber, cb]);
   subscribeToTickerOnWebsocket(ticker);
 };
+
 export const unsubscribeFromTicker = ticker => {
   //Отписка от тикера , просто удаляет тикер из tickerHandler
   tickersHandlers.delete(ticker);
